b02c9122df34a608f6aa98f9056e0d7b5dea541d
Jeremy Warren <jeremyawarren15@gmail.com>
2020-01-23 08:33:26 -0500

    fixed cors issue

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./typedefs");
 const resolvers = require("./resolvers");
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  cors: { origin: "*", credentials: true },
+  typeDefs,
+  resolvers
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
